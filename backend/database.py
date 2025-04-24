09ddfd1214526e2e99bced02fca865e5119dabe1
AndresOrtega-tech <tamezandres191@gmail.com>
2025-04-24 16:34:48 -0600

    se modifico la url de la base de datos, ahora es correcta

--- backend/database.py
+++ backend/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://user:password@localhost/proyectofinal"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:@localhost:3306/proyectofinal"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
